5226e5d1ca97c1dbcc8d8e457f7ae2a39e79719f
Zachary Yedidia <zyedidia@gmail.com>
2025-08-11 22:02:08 -0700

    Fix typo

--- core/include/lfi_core.h
+++ core/include/lfi_core.h
@@ -337,7 +337,7 @@ void
 lfi_trampoline_struct(void) __asm__("lfi_trampoline_struct");
 
 void
-lfi_clone(struct LFIBox *box, struct LFIContext **ctxp)
+lfi_clone(struct LFIBox *box, struct LFIContext **ctxp);
 
 #define LFI_X(x, y)  x##y
 #define LFI_XX(x, y) LFI_X(x, y)
