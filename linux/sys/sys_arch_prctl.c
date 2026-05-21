ef5fa758859e20fb3f7631851254168a221ae462
Zachary Yedidia <zyedidia@gmail.com>
2026-05-21 15:34:52 -0700

    x64: allow arch_prctl in sys_minimal to set tp

--- linux/sys/sys_arch_prctl.c
+++ linux/sys/sys_arch_prctl.c
@@ -8,8 +8,8 @@ sys_arch_prctl(struct LFILinuxThread *t, int code, lfiptr addr)
 #if defined(LFI_ARCH_X64)
     switch (code) {
     case LINUX_ARCH_SET_FS:
-#ifdef SYS_MINIMAL
-        // No TLS support in sys_minimal mode.
+#if defined(SYS_MINIMAL) && !defined(CTXREG)
+        // No TLS support in sys_minimal mode without CTXREG.
         return -LINUX_ENOSYS;
 #else
         lfi_ctx_set_tp(t->ctx, addr);
